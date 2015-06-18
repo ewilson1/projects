9e550dde4710394ff289f595378a8765bdd4ab93
ewilson1 <ewilson1@fullsail.edu>
2015-06-18 15:38:59 -0400

    3 scroll view app update

---
+++ 3-scroll.views/Resources/app.js
@@ -0,0 +1,47 @@
+var pWidth = Ti.Platform.displayCaps.platformWidth;
+var pHeight = Ti.Platform.displayCaps.platformHeight;
+var itemCount = 30;
+var rowCount = 3;
+var margin = 10;
+var trueCanvasWidth = (pWidth - margin*4);
+var size = (trueCanvasWidth/3);
+	
+
+var mainWin = Ti.UI.createWindow({
+	backgroundColor: "#fff",
+	layout: "horizontal"
+});
+
+var border = Ti.UI.createView({
+	backgroundColor: "#cecece",
+	height: 1,
+	width: pWidth,
+	top: 20
+});
+
+var viewContainer = Ti.UI.createScrollView({
+	top: 0,
+	layout: "horizontal",
+	width: pWidth,
+	contentWidth: pWidth,
+	height: pHeight-border.height-border.top,
+	showVerticalScrollIndicator: true,
+	backgroundColor: "#fef",
+});
+
+for(var i=0; i<itemCount; i++){
+	var view = Ti.UI.createView({
+		backgroundColor: "#33ccff",
+		top: margin,
+		left: margin,
+		width: size,
+		height: size,
+	});
+	var text = Ti.UI.createLabel({text: i+1, color: "#fff"});
+	view.add(text);
+	viewContainer.add(view);
+}
+
+mainWin.add(border);
+mainWin.add(viewContainer);
+mainWin.open();
