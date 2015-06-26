badde3115a07523cc135517867d0b01221480ff6
ewilson1 <ewilson1@fullsail.edu>
2015-06-25 21:01:39 -0400

    Week 4 start over with new menu

--- wilson_eric_project4/Resources/app.js
+++ wilson_eric_project4/Resources/app.js
@@ -1,28 +1,106 @@
-Titanium.UI.setBackgroundImage; "Weather-Background.jpg";
+Ti.UI.setBackgroundColor("fff");
 
-//Data for the table view
-var menu = [{title: "Random Weather Images"}, {title: "The Weatherman's Family"}, {title: "Interactive Weather"}];
-
+//create a window to hold the Navigation window
 var mainWindow = Ti.UI.createWindow({
-	title: "Eric Wilson's Weather",
-		
+	title: "Eric Wilson's Weather Menu",
+	backgroundImage: "Weather-Background.jpg"	
 });
 	var navWindow = Ti.UI.iOS.createNavigationWindow({
-		window: mainWindow,
-		
+		window: mainWindow
 	});
 
-var weatherTable = Ti.UI.createTableView({
-	top: navWindow.height + navWindow.top,
-	backgroundImage: "Weather-Background.jpg"
+//declare a variable to create the view, which will hold the text
+var openView = Ti.UI.createView({
+	backgroundColor: "blue",
+	border: 1,
+	borderRadius: 20,
+	left: 50,
+	right: 50,
+	height: 100,
+	top: 100,
 });
 
-if(Ti.Platform.name === "iPhone OS"){
-	weatherTable.style = Ti.UI.iPhone.TableViewStyle.GROUPED;
-};
+//declare a variable to create a label for the text inside the window (view) above
+var openText = Ti.UI.createLabel({
+	text: "Weather Gallery Page",
+	color: "white",
+	textAlign: "center",
+	font: {fontSize: 20, fontfamily: "Arial"},
+});
 
-weatherTable.setData(menu);
+//now add the text (label) to the first view
+openView.add(openText);
 
-mainWindow.add(weatherTable);
-mainWindow.open();
+var openView1 = Ti.UI.createView({
+	backgroundColor: "blue",
+	border: 1,
+	borderRadius: 20,
+	top: openView.top + openView.height + 10,
+	left: 50,
+	right: 50,
+	height: 100,
+	
+});
+
+//declare a variable to create a label for the text inside the window (view) above
+var openText1 = Ti.UI.createLabel({
+	text: "Wilson's Weather Team",
+	color: "white",
+	textAlign: "center",
+	font: {fontSize: 20, fontfamily: "Arial"},
+});
+
+//now add the text (label) to the view
+openView1.add(openText1);
+
+var openView2 = Ti.UI.createView({
+	backgroundColor: "blue",
+	border: 1,
+	borderRadius: 20,
+	top: openView1.top + openView1.height + 10,
+	left: 50,
+	right: 50,
+	height: 100,
+	
+});
+
+//declare a variable to create a label for the text inside the window (view) above
+var openText2 = Ti.UI.createLabel({
+	text: "Custom Weather Page",
+	color: "white",
+	textAlign: "center",
+	font: {fontSize: 20, fontfamily: "Arial"},
+});
+
+var openText3 = Ti.UI.createLabel({
+	text: "VFW Term 1506//Eric Wilson",
+	top: openView2.top + openView2.height + 50,
+	color: "white",
+	textAlign: "center",
+	font: {fontSize: 15, fontfamily: "Arial"},
+	
+});
+
+var openText4 = Ti.UI.createLabel({
+	text: "Click Below To Get Started",
+	top: 50,
+	color: "white",
+	textAlign: "center",
+	font: {fontSize: 25, fontfamily: "Arial"},
+	
+});
+
+//now add the text (label) to the view
+openView2.add(openText2);
+
+
+//Now, require both .js files here on the bootstrap page, in backwards order...
+//var pictures = require("FullScreen");
+
+//var weather = require("Weather_Gallery");
+
+//add the View to the main window
+mainWindow.add(openView, openView1, openView2, openText3, openText4);
+
+//open the main window
 navWindow.open();
