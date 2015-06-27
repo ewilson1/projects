755d4e2dab17386f547729225fc8e3c5111bc0ee
ewilson1 <ewilson1@fullsail.edu>
2015-06-27 16:43:10 -0400

    Week 4 buttons all work and point to pages

--- wilson_eric_project4/Resources/custom.js
+++ wilson_eric_project4/Resources/custom.js
@@ -1,22 +1,14 @@
 //declare a variable to run a function, then another variable for the window
 var customPage = function(){
 	var customWindow = Ti.UI.createWindow({
-		title: "Custom Weather"
+		title: "Weather Fun",
 	});
-	customWindow.add(bgImage);
 	//don't forget to call the navigation window from the first page and load the new variable window name
 	navWindow.openWindow(customWindow);
+
+//Start building the platform to meet the requirements of this page
+
+
 };
 
-//add event listener here, there will be another event listener at the bottom of the page
 openView2.addEventListener("click", customPage);
-
-//set the parameters for the background page
-var pWidth = Ti.Platform.displayCaps.platformWidth;
-var pHeight = Ti.Platform.displayCaps.platformHeight;
-
-var bgImage = Ti.UI.createImageView({
-	image: "Weather-Background.jpg",
-	width: pWidth,
-	height: pHeight
-});
